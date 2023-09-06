34bd1443bf1439ee77d5baf8a542f953112c8963
David-JPena <jimenezpenadavid978@gmail.com>
2023-09-06 11:01:23 -0500

    versio3_3

--- listar.php
+++ listar.php
@@ -26,17 +26,18 @@ $result = pg_query($con, $query);
                 </tr>
             </thead>
             <tbody>
-                <?php
-                while ($row = pg_fetch_assoc($result)) {
-                    echo "<tr>";
-                    echo "<td>" . $row['nro_documento'] . "</td>";
-                    echo "<td>" . $row['nombre'] . "</td>";
-                    echo "<td>" . $row['apellidos'] . "</td>";
-                    echo "<td>" . $row['direccion'] . "</td>";
-                    echo "<td>" . $row['celular'] . "</td>";
-                    echo "</tr>";
-                }
-                ?>
+            <?php
+            while ($row = pg_fetch_assoc($result)) {
+                echo "<tr>";
+                echo "<td>" . $row['id'] . "</td>"; // Cambia 'id' por el nombre correcto de la columna en tu tabla.
+                echo "<td>" . $row['nro_documento'] . "</td>";
+                echo "<td>" . $row['nombre'] . "</td>";
+                echo "<td>" . $row['apellidos'] . "</td>";
+                echo "<td>" . $row['direccion'] . "</td>";
+                echo "<td>" . $row['celular'] . "</td>";
+                echo "</tr>";
+            }
+            ?>
             </tbody>
         </table>
     </div>

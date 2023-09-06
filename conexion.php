f1ee6b2e8f1389c7de00e4e07fb2d47a4d1db62a
David-JPena <jimenezpenadavid978@gmail.com>
2023-09-06 10:05:33 -0500

    sesion2_v1

--- conexion.php
+++ conexion.php
@@ -2,11 +2,11 @@
 
 	function conexion(){
 
-	$host = "host=xxxx";
-	$port = "port=xxxx";
-	$dbname = "dbname=xxxx";
-	$user = "user=xxxx";
-	$password = "password=xxxx";
+	$host = "host=containers-us-west-69.railway.app";
+	$port = "port=6527";
+	$dbname = "dbname=railway";
+	$user = "user=postgres";
+	$password = "password=e9zJugXcUKq9pd1Umdi8";
 
 	$db = pg_connect("$host $port $dbname $user $password");
 
